845026ec2a62432777c9c4cf04c35971a1d174fd
Upari <panqiyu521@gmail.com>
2026-07-21 11:42:38 +0800

    feat: complete virtual differential drive robot

---
+++ src/robot_bringup/launch/virtual_robot.launch.py
@@ -0,0 +1,69 @@
+import os
+
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+from ament_index_python import get_package_share_directory
+
+
+def generate_launch_description():
+    robot_description_share = get_package_share_directory(
+        "robot_description"
+    )
+
+    xacro_file = os.path.join(
+        robot_description_share,
+        "urdf",
+        "robot.urdf.xacro"
+    )
+
+    robot_description = ParameterValue(
+        Command([
+            "xacro",
+            " ",
+            xacro_file
+        ]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": robot_description
+            }
+        ]
+    )
+
+    virtual_diff_drive_node = Node(
+        package="virtual_robot_base",
+        executable="virtual_diff_drive_node",
+        name="virtual_diff_drive_node",
+        output="screen"
+    )
+
+    vritual_lidar_node = Node(
+        package="virtual_robot_sensors",
+        executable="virtual_lidar_node",
+        name="virtual_lidar_node",
+        output="screen"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen"
+    )
+    
+    return LaunchDescription([
+        robot_state_publisher_node,
+        virtual_diff_drive_node,
+        vritual_lidar_node,
+        rviz_node
+    ])
