700be5cdc8e3108f813a22edfbdd495c23389a63
Upari <panqiyu521@gmail.com>
2026-07-21 10:02:47 +0800

    urdf change to xacro

---
+++ src/robot_description/launch/display.launch.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+def generate_launch_description():
+    package_share = get_package_share_directory(
+        "robot_description"
+    )
+
+    xacro_file = os.path.join(
+        package_share,
+        "urdf",
+        "robot.urdf.xacro"
+    )
+
+    robot_description = ParameterValue(
+        Command([
+            "xacro",
+            " ",
+            xacro_file
+        ]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": robot_description
+            }
+        ]
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen"
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz_node
+    ])
